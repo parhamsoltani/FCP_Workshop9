3450cd58684b81b8beb32f90b807f6186d7361bb
parhamsoltani <70743729+parhamsoltani@users.noreply.github.com>
2023-05-03 14:43:00 +0330

    Create gcd.cpp

---
+++ Session9/gcd.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int GCD(int a , int b)
+{
+    if (a==0){
+    	return b;
+	}
+	if (b==0){
+		return a;
+	}
+	return GCD(b,a%b);
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<GCD(a,b);
+    return 0;
+}
